ad6e7c6a30106f90116750ddc705f4e59c61c7c1
UyenDo <uyendo>
2022-08-16 16:14:24 +0700

    no message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="container mt-2">
+      <h4>Test</h4>
+      <div className="form-group mt-4 mb-2">
+        Demo for build
+      </div>
+    </div>
+  )
+}
+
+export default App
